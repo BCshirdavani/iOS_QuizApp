e4366402bd4c6c2270f05ce7461c83a93bf1aa00
shiMac <beau.shirdavani@bellevuecollege.edu>
2018-06-25 22:46:05 -0700

    1 question
    todo: make rest of the question work as well

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -18,14 +18,28 @@ class ViewController: UIViewController {
     @IBOutlet var progressBar: UIView!
     @IBOutlet weak var progressLabel: UILabel!
     
+    let allQuastions = QuestionBank()
+    var pickedAnswer : Bool = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let firstQuestion = allQuastions.list[0]            // make first question
+        questionLabel.text = firstQuestion.questionText     // display first question
         
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        print(sender.tag)
+        if sender.tag == 1 {
+            pickedAnswer = true
+        }
+        else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        checkAnswer()
     }
     
     
@@ -40,6 +54,13 @@ class ViewController: UIViewController {
     
     
     func checkAnswer() {
+        let correctAnswer = allQuastions.list[0].answer
+        if correctAnswer == pickedAnswer{
+            print("You got it!")
+        }
+        else{
+            print("wrong")
+        }
         
     }
     
